fe805070c644211d3eb33360a7d1cc8ca4d51761
Leyukarii <leyukari@hotmail.com>
2024-10-23 17:02:30 -0300

    responsividade com tailwind

--- components/Cadastro.vue
+++ components/Cadastro.vue
@@ -1,38 +1,50 @@
 <template>
-  <section class="flex py-36">
+  <section class="flex flex-col py-8 md:py-36">
     <div class="flex flex-col items-center w-full">
-      <h1 class="text-4xl mb-4 text-center text-[#044c8c]">
-        {{ userStore.currentUser.id ? "Edição de Usuário" : "Cadastro de Usuário" }}</h1>
-      <form class="grid gap-4 bg-white p-8 rounded-lg shadow-md w-full max-w-sm"
-      @submit.prevent="userStore.currentUser.id ? updateUser() : addUser()">
+      <h1 class="text-3xl md:text-4xl mb-4 text-center text-[#044c8c]">
+        {{ userStore.currentUser.id ? "Edição de Usuário" : "Cadastro de Usuário" }}
+      </h1>
+      <form class="grid gap-4 bg-white p-8 rounded-lg shadow-md w-full max-w-sm mx-auto" 
+            @submit.prevent="userStore.currentUser.id ? updateUser() : addUser()">
         <label class="mb-1 text-[#626569]">Nome Completo</label>
-        <input class="rounded-md border border-white p-4 shadow-md transition-all duration-300 text-base font-sans mb-4 text-[#626569] focus:outline-none focus:border-[#008edc] focus:shadow-lg hover:shadow-lg"
-        type="text" v-model="userStore.currentUser.name" name="name" id="name">
+        <input class="rounded-md border p-4 shadow-md transition-all duration-300 text-base font-sans mb-4 text-[#626569] 
+              focus:outline-none focus:border-[#008edc] focus:shadow-lg hover:shadow-lg hover:border-[#008edc]"
+               type="text" v-model="userStore.currentUser.name" name="name" id="name">
 
         <label class="mb-1 text-[#626569]">E-mail</label>
-        <input class="rounded-md border border-white p-4 shadow-md transition-all duration-300 text-base font-sans mb-4 text-[#626569] focus:outline-none focus:border-[#008edc] focus:shadow-lg hover:shadow-lg"
-        type="email" v-model="userStore.currentUser.email" name="email" id="email">
+        <input class="rounded-md border p-4 shadow-md transition-all duration-300 text-base font-sans mb-4 text-[#626569] 
+              focus:outline-none focus:border-[#008edc] focus:shadow-lg hover:shadow-lg hover:border-[#008edc]"
+               type="email" v-model="userStore.currentUser.email" name="email" id="email">
 
         <label class="mb-1 text-[#626569]">Senha</label>
-        <input class="rounded-md border border-white p-4 shadow-md transition-all duration-300 text-base font-sans mb-4 text-[#626569] focus:outline-none focus:border-[#008edc] focus:shadow-lg hover:shadow-lg"
-        type="password" v-model="userStore.currentUser.password" name="password" id="password">
+        <input class="rounded-md border p-4 shadow-md transition-all duration-300 text-base font-sans mb-4 text-[#626569] 
+        focus:outline-none focus:border-[#008edc] focus:shadow-lg hover:shadow-lg hover:border-[#008edc]"
+               type="password" v-model="userStore.currentUser.password" name="password" id="password">
 
-        <button class="w-full max-w-xs mx-auto block px-8 py-2 bg-[#006eb6] rounded-md text-white text-center text-base shadow-lg transition-all duration-300 hover:bg-[#008edc] hover:scale-110 focus:outline-none font-sans cursor-pointer">
-          {{ userStore.currentUser.id ? "Atualizar" : "Cadastrar" }}</button>
+        <button class="w-full max-w-xs mx-auto block px-8 py-2 bg-[#006eb6] rounded-md text-white text-center text-base shadow-lg transition-all duration-300 hover:bg-[#008edc] hover:scale-110 focus:outline-none">
+          {{ userStore.currentUser.id ? "Atualizar" : "Cadastrar" }}
+        </button>
       </form>
 
-      <button class="w-full max-w-xs mx-auto block px-8 py-2 bg-[#006eb6] rounded-md text-white text-center text-base shadow-lg transition-all duration-300 hover:bg-[#008edc] hover:scale-110 focus:outline-none font-sans cursor-pointer mt-5" @click="toggleUserList">Listar Usuários</button>
+      <button class="w-full max-w-xs mx-auto block px-8 py-2 bg-[#006eb6] rounded-md text-white text-center text-base shadow-lg transition-all duration-300 hover:bg-[#008edc] hover:scale-110 focus:outline-none font-sans cursor-pointer mt-5" 
+              @click="toggleUserList">
+        Listar Usuários
+      </button>
 
-      <div v-if="showUserList && userStore.users.length">
+      <div v-if="showUserList && userStore.users.length" class="w-full max-w-md mx-auto">
         <h1 class="text-3xl mb-4 mt-5 text-center text-[#044c8c]">Usuários Cadastrados</h1>
         <ul class="list-none p-0 m-0 text-[#044c8c]"> 
-          <li class="bg-white p-2 mb-2 text-[#626569] rounded-lg shadow-md m-0 text-[#044c8c]"
-            v-for="user in userStore.users" :key="user.id">
+          <li class="bg-white p-2 mb-2 text-[#626569] rounded-lg shadow-md"
+              v-for="user in userStore.users" :key="user.id">
             <div class="flex justify-between items-center w-full">
               <span>{{ user.name }} - {{ user.email }}</span>
               <div class="flex gap-2">
-                <button  class="ml-2 px-2.5 py-1.5 border-none rounded-md bg-[#006eb6] text-white cursor-pointer hover:bg-[#008edc]" @click="editUser(user.id)">Editar</button>
-                <button  class="ml-2 px-2.5 py-1.5 border-none rounded-md bg-[#006eb6] text-white cursor-pointer hover:bg-[#008edc]" @click="deleteUser(user.id)">Deletar</button>
+                <button class="ml-2 px-2.5 py-1.5 border-none rounded-md bg-[#006eb6] text-white cursor-pointer hover:bg-[#008edc]" @click="editUser(user.id)">
+                  Editar
+                </button>
+                <button class="ml-2 px-2.5 py-1.5 border-none rounded-md bg-[#006eb6] text-white cursor-pointer hover:bg-[#008edc]" @click="deleteUser(user.id)">
+                  Deletar
+                </button>
               </div>
             </div>
           </li>
--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,18 @@
 <template>
   <header>
-    <nav class="flex items-center justify-between p-4 shadow-md">
+    <nav class="flex items-center justify-between p-4 shadow-md bg-white">
       <div class="logo py-2.5">
-        <img class="w-[150px]" src="@/assets/logo.png" alt="Setup Tecnologia">
+        <img class="w-[150px] md:w-[150px]" src="@/assets/logo.png" alt="Setup Tecnologia">
       </div>
-      <div class="block px-8 py-2 bg-[#006eb6] rounded-md text-white text-center text-base shadow-lg transition-all duration-300 border-none font-sans cursor-pointer hover:bg-[#008edc]">Entrar</div>
+      <div class="block px-4 py-2 bg-[#006eb6] rounded-md text-white text-center text-base shadow-lg transition-all duration-300 border-none font-sans cursor-pointer hover:bg-[#008edc] md:px-8">
+        Entrar
+      </div>
     </nav>
   </header>
 </template>
 
 <script>
-export default{
+export default {
   name: "Header"
 };
 </script>
