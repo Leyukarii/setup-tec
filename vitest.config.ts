38ce071b09781afdf8e99d09b5eef74b1ccaccad
Leyukarii <55262488+Leyukarii@users.noreply.github.com>
2024-10-24 16:17:27 -0300

    Adiciona arquivos de configuração de testes e vitest

---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import Vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [Vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),    
+      '~': path.resolve(__dirname, './'),    
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',  
+  },
+});
+// vitest.config.ts
+import { defineConfig } from 'vitest/config';
+import Vue from '@vitejs/plugin-vue';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [Vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),    
+      '~': path.resolve(__dirname, './'),    
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',  
+  },
+});
